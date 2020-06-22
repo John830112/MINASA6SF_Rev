13cc829ff434008a49ab92e3805abde6cf2acd08
yoonbyoungshik <ensoul0@gmail.com>
2020-06-23 00:07:40 +0900

    application단위에서 VM 인스턴스...

--- MINASA6SF_Rev/App.xaml.cs
+++ MINASA6SF_Rev/App.xaml.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using MINASA6SF_Rev.ViewModels;
+using MINASA6SF_Rev.Views;
 
 namespace MINASA6SF_Rev
 {
@@ -13,5 +15,6 @@ namespace MINASA6SF_Rev
     /// </summary>
     public partial class App : Application
     {
+        MainPanelViewModel mainPanelViewModel = new MainPanelViewModel();
     }
 }
